b105a2632848386db9a21479637aa60a8132f750
yuricks7 <47909257+yuricks7@users.noreply.github.com>
2021-01-21 18:32:32 +0900

    removed the extra loop block.

--- Calendar/Main/AddEvents.js
+++ Calendar/Main/AddEvents.js
@@ -10,55 +10,51 @@ function ExportToCalendar() {
   const actionLog = new LogCalendar('行動ログ');
   const  sleepLog = new LogCalendar('睡眠ログ');
 
+  // 時限タイマーをセット
+  const timer = new Timer();
+  timer.setMax(5.5); // 少し余裕を持って5分半とする
+  timer.start();
+
   // シートのデータをカレンダーに転記
-  // 
+  //
   // @note
   //   1度に転記する数が多いとサーバーエラーで止められることがあるため、
   //   途中終了しないようにtry~catch構文の中で処理する
   //
-  const timer = new Timer();
-  timer.start();
-
   try {
 
-    // 時限タイマーをセット
-    const LIMITED_TIME = 5.5 * 60 * 1000; // 少し余裕を持って5分半とする
     var isCopied = []; // シート記録用
+    for (var i = rows.firstData - 1; i < values.length; i++) {
+      var rowValues = values[i];
 
-    while (true) {
-
-      // 処理を開始
-      for (var i = rows.firstData - 1; i < values.length; i++) {
-        var rowValues = values[i];
-
-        // レコードの内容によっては処理を飛ばす
-        if (rowValues[cols.finishTime - 1] === '') {
-          isCopied.push('');
-          continue;
-        }
-
-        if (rowValues[cols.isCopied - 1]) {
-          isCopied.push(true);
-          continue;
-        }
-
-        // 睡眠ログとそれ以外を分けて記録
-        var logContent = new LogContent(rowValues, cols);
-        switch (logContent.task) {
-          case '睡眠':
-            addLog(sleepLog, logContent);
-            break;
-
-          default:
-            addLog(actionLog, logContent);
-            break;
-        }
-
-        isCopied.push(true);
+      // レコードの内容によっては処理を飛ばす
+      if (rowValues[cols.finishTime - 1] === '') {
+        isCopied.push('');
+        continue;
       }
 
+      if (rowValues[cols.isCopied - 1]) {
+        isCopied.push(true);
+        continue;
+      }
+
+      // 睡眠ログとそれ以外を分けて記録
+      var logContent = new LogContent(rowValues, cols);
+      switch (logContent.task) {
+        case '睡眠':
+          addLog(sleepLog, logContent);
+          break;
+
+        default:
+          addLog(actionLog, logContent);
+          break;
+      }
+
+      isCopied.push(true);
+
+      // 時間制限対策
       console.log(timer.rap());
-      if (timer.getRapTime() > LIMITED_TIME) break;
+      if (timer.getRapTime() > timer.max) break;
     }
 
   } catch(e) {
@@ -100,7 +96,7 @@ function addLog(logCalendar, logContent) {
  *   https://maetoo11.hatenablog.com/entry/2017/05/10/123140
  *   > 各行の0番目の値を取り出した配列を作る、行の1番目の値を取り出した配列を作る…を繰り返して、新しい2次元配列を作成。
  * 
- * @param {[][]} src 転置前の配列
+ * @param {[[boolean]]} src 転置前の配列
  *
  * @return {[][]} 転置後の二次元配列
  */
