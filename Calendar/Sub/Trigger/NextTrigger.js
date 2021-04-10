fd80e2c47338f464c7ddd835f3ef809c54056942
yuricks7 <47909257+yuricks7@users.noreply.github.com>
2021-04-10 13:10:50 +0900

    added process to delete all triggers of current project.

--- Calendar/Sub/Trigger/NextTrigger.js
+++ Calendar/Sub/Trigger/NextTrigger.js
@@ -13,10 +13,19 @@ var NextTrigger = function(funcName, afterByMinutes) {
 
 /**
  * トリガーを設定する
- *
+ * 
  * - 【参考】
  *   - 高橋宣成（2020）『[詳解! Google Apps Script完全入門[第2版] ~GoogleアプリケーションとGoogle Workspaceの最新プログラミングガイド](https://amzn.to/3rSFq69)』秀和システム 第1版第1刷（p.547）
  */
 NextTrigger.prototype.build = function() {
-  return ScriptApp.newTrigger(this.funcName).timeBased().after(this.afterByMilliSec).create();
+
+  // いったん全部消す
+  // > Gets all installable triggers **associated with the current project and current user**.
+  var triggers = ScriptApp.getProjectTriggers();
+  for (var i = 0; i < triggers.length; i++) {
+    ScriptApp.deleteTrigger(triggers[i]);
+  }
+
+  // 新しく追加
+  return nextTrigger = ScriptApp.newTrigger(this.funcName).timeBased().after(this.afterByMilliSec).create();
 }
